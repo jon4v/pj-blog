5d1fc601df4f71738eb7aa34c7a68558259e9f22
joenavre <joel.navea.r@gmail.com>
2024-03-09 21:20:08 -0300

    minor text update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Logo from '../img/logo.png'
+import React from "react";
+import Logo from "../img/logo.png";
 
 const Footer = () => {
-    return (
-        <footer>
-            <img src={Logo} alt=""/>
-            <span>
-                Made by <b>joe</b> and <b>React.js</b>
-            </span>
-        </footer>
-    );
-}
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Made by <b>Joe</b>
+      </span>
+    </footer>
+  );
+};
 
 export default Footer;
